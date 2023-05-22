cf566633004eb0f6fa08324af4b06e8e0337373f
Kooreis <kooreis.official@gmail.com>
2023-05-22 10:01:58 -0500

    This update initializes a 2D array 'dp' with dimensions (n+2)x(n+2) and fills it with zeros. This array will be used to store the maximum number of coins that can be obtained by bursting balloons from i to j.

--- solution.ts
+++ solution.ts
@@ -1,8 +1,1 @@
-function maxCoins(nums: number[]): number {
-    const n = nums.length;
-    const newNums = new Array(n + 2);
-    newNums[0] = newNums[n + 1] = 1;
-    for (let i = 1; i <= n; i++) {
-        newNums[i] = nums[i - 1];
-    }
-}
+const dp = Array.from(new Array(n + 2), () => new Array(n + 2).fill(0));
