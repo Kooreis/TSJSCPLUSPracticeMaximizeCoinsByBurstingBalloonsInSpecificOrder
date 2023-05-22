53698b0df965a9b7ade57a8429cb81421f77ffa9
Kooreis <kooreis.official@gmail.com>
2023-05-22 10:01:58 -0500

    This update defines the function maxCoins, which calculates the maximum number of coins that can be obtained from a vector of integers. It also initializes a 2D vector dp with the size of nums and fills it with zeros.

--- solution.cpp
+++ solution.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
-using namespace std;
+int maxCoins(vector<int>& nums) {
+    int n = nums.size();
+    nums.insert(nums.begin(), 1);
+    nums.insert(nums.end(), 1);
+    vector<vector<int>> dp(nums.size(), vector<int>(nums.size(), 0));
